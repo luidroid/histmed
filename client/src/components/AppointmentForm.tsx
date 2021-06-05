8cf4b7e99523fdcc260b4093f99b04ae9465b2d2
luisamoroso <luis.amoroso@hotmail.com>
2021-06-05 07:03:17 +0200

    added form appointments with logic

--- client/src/components/AppointmentForm.tsx
+++ client/src/components/AppointmentForm.tsx
@@ -5,8 +5,17 @@ import { Formik, Form } from "formik";
 import * as yup from "yup";
 
 import { useGlobalStyles } from "../styles/globalStyles";
-import { Appointment, AppointmentType, Patient } from "../models/patient";
-import { initAppointment, initPatient } from "../api/patientService";
+import {
+  Appointment,
+  AppointmentType,
+  CustomError,
+  Patient,
+} from "../models/patient";
+import {
+  initAppointment,
+  initCustomError,
+  initPatient,
+} from "../api/patientService";
 import PersonInfoCompact from "../components/PersonInfoCompact";
 
 import Box from "@material-ui/core/Box";
@@ -28,9 +37,12 @@ import {
 import { DropzoneArea } from "material-ui-dropzone";
 import PatientHistoryGeneric from "./PatientHistoryGeneric";
 import { APPOINTMENTS_URL, PATIENTS_URL } from "../constants/constants";
+import MenuItem from "@material-ui/core/MenuItem";
+import CustomAlertError from "./CustomAlertError";
+import Loading from "./Loading";
 
 const validationSchema = yup.object({
-  type: yup.string(),
+  category: yup.string(),
   scheduled: yup.date(),
   title: yup.string().required("Motivo es requerido"),
   description: yup.string(),
@@ -48,16 +60,25 @@ export default function AppointmentForm({ edit }: Props) {
   const history = useHistory();
   const { id } = useParams<{ id: string }>();
   const patientUrl = `${PATIENTS_URL}/${id}`;
-  const appointmentUrl = `${APPOINTMENTS_URL}`;
+  const appointmentsUrl = `${APPOINTMENTS_URL}`;
   const [patient, setPatient] = useState<Patient>(initPatient);
   const [appointment, setAppointment] = useState<Appointment>(initAppointment);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [customError, setCustomError] = useState<CustomError>(initCustomError);
 
   useEffect(() => {
+    setLoading(true);
     (async () => {
+      setError(false);
       try {
         const { data } = await axios.get<Patient>(patientUrl);
         setPatient(data);
+        setLoading(false);
       } catch (error) {
+        setLoading(false);
+        setCustomError(error);
+        setError(true);
         console.log(error);
       }
     })();
@@ -65,11 +86,16 @@ export default function AppointmentForm({ edit }: Props) {
 
   useEffect(() => {
     if (edit) {
+      setLoading(true);
       (async () => {
         try {
           const { data } = await axios.get<Appointment>(patientUrl);
           setAppointment(data);
+          setLoading(false);
         } catch (error) {
+          setLoading(false);
+          setCustomError(error);
+          setError(true);
           console.log(error);
         }
       })();
@@ -77,217 +103,230 @@ export default function AppointmentForm({ edit }: Props) {
   }, [edit, patientUrl]);
 
   return (
-    <Formik
-      initialValues={appointment}
-      validationSchema={validationSchema}
-      enableReinitialize
-      onSubmit={(values, actions) => {
-        if (!edit) {
-          values.person = id;
-        }
-        console.log(values);
-
-        if (edit) {
-          (async () => {
-            try {
-              await axios.put<Appointment>(patientUrl, values);
-              history.push(patientUrl);
-            } catch (error) {
-              console.log(error);
-            }
-          })();
-        } else {
-          (async () => {
-            try {
-              await axios.post<Appointment>(patientUrl, values);
-              history.push(patientUrl);
-            } catch (error) {
-              console.log(error);
-            }
-          })();
-        }
-      }}
-    >
-      {(formik) => (
-        <Form className={globalClasses.root} autoComplete="off">
-          <Grid container spacing={1}>
-            <Grid item xs={12} md={12} lg={8}>
-              <Paper className={globalClasses.paper}>
-                <Typography component="h2" variant="h6" color="primary">
-                  {edit ? "Editar" : "Nueva"} consulta
-                </Typography>
-                <FormControl fullWidth className={globalClasses.root}>
-                  <TextField
-                    variant="filled"
-                    id="title"
-                    name="title"
-                    label="Motivo *"
-                    value={formik.values.title}
-                    onChange={formik.handleChange}
-                    error={formik.touched.title && Boolean(formik.errors.title)}
-                    helperText={formik.touched.title && formik.errors.title}
-                  />
-
-                  <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                    <KeyboardDatePicker
-                      id="scheduled"
-                      name="scheduled"
-                      inputVariant="filled"
-                      variant="inline"
-                      format="dd/MM/yyyy"
-                      margin="normal"
-                      label="Fecha"
-                      placeholder="dd/mm/yyyy"
-                      value={formik.values.scheduled}
-                      onChange={(value) =>
-                        formik.setFieldValue("scheduled", value)
-                      }
-                    />
-                  </MuiPickersUtilsProvider>
-
-                  <FormControl
-                    variant="filled"
-                    className={globalClasses.spacing}
-                  >
-                    <InputLabel htmlFor="outlined-age-native-simple">
-                      Tipo
-                    </InputLabel>
-                    <Select
-                      value={formik.values.category}
-                      label="Tipo"
-                      name="type"
-                      onChange={formik.handleChange}
-                    >
-                      <option value={AppointmentType.PreliminaryTalk}>
-                        Charla preliminar
-                      </option>
-                      <option value={AppointmentType.Surgery}>Cirugía</option>
-                      <option value={AppointmentType.Check}>Revisión</option>
-                      <option value={AppointmentType.Other}>Otro</option>
-                    </Select>
-                  </FormControl>
-
-                  <TextField
-                    variant="filled"
-                    id="description"
-                    name="description"
-                    label="Description"
-                    multiline
-                    rows={4}
-                    value={formik.values.description}
-                    onChange={formik.handleChange}
-                  />
-                  <TextField
-                    variant="filled"
-                    id="analysis"
-                    name="analysis"
-                    label="Exámen físico"
-                    multiline
-                    rows={4}
-                    value={formik.values.analysis}
-                    onChange={formik.handleChange}
-                  />
-                  <TextField
-                    variant="filled"
-                    id="plan"
-                    name="plan"
-                    label="Plan"
-                    multiline
-                    rows={10}
-                    value={formik.values.plan}
-                    onChange={formik.handleChange}
-                  />
-                  <TextField
-                    variant="filled"
-                    id="comment"
-                    name="comment"
-                    label="Observaciones"
-                    multiline
-                    rows={4}
-                    value={formik.values.comment}
-                    onChange={formik.handleChange}
-                  />
-                  <TextField
-                    variant="filled"
-                    id="recommendation"
-                    name="recommendation"
-                    label="Recomendaciones"
-                    multiline
-                    rows={4}
-                    value={formik.values.recommendation}
-                    onChange={formik.handleChange}
-                  />
-                </FormControl>
-              </Paper>
-              <Paper className={globalClasses.paper}>
-                <Typography component="h2" variant="h6" color="primary">
-                  Fotos (Antes)
-                </Typography>
-                <DropzoneArea
-                  acceptedFiles={["image/*"]}
-                  dropzoneText={"Drag and drop an image here or click"}
-                  onChange={(files) => console.log("Files:", files)}
-                  filesLimit={1}
-                />
-              </Paper>
-              <Paper className={globalClasses.paper}>
-                <Typography component="h2" variant="h6" color="primary">
-                  Fotos (Después)
-                </Typography>
-                <DropzoneArea
-                  acceptedFiles={["image/*"]}
-                  dropzoneText={"Drag and drop an image here or click"}
-                  onChange={(files) => console.log("Files:", files)}
-                  filesLimit={2}
-                />
-              </Paper>
-              <Paper className={globalClasses.paper}>
-                <Typography component="h2" variant="h6" color="primary">
-                  Archivos
-                </Typography>
-                ^{" "}
-                <DropzoneArea
-                  acceptedFiles={["image/*"]}
-                  dropzoneText={"Drag and drop an image here or click"}
-                  onChange={(files) => console.log("Files:", files)}
-                  filesLimit={4}
-                  showFileNames={true}
-                />
-              </Paper>
-              <Box className={globalClasses.spacing}>
-                <Button
-                  color="primary"
-                  variant="contained"
-                  size="large"
-                  type="submit"
-                >
-                  Guardar
-                </Button>
-                <Button
-                  variant="outlined"
-                  size="large"
-                  component={RouterLink}
-                  to={`/questionnaire`}
-                >
-                  Cancelar
-                </Button>
-              </Box>
-            </Grid>
-            <Grid item xs={12} md={12} lg={4}>
-              <PersonInfoCompact person={patient}></PersonInfoCompact>
-
-              <Paper className={globalClasses.paper}>
-                <Typography component="h2" variant="h6" color="primary">
-                  Antecedentes
-                </Typography>
-                <PatientHistoryGeneric
-                  records={patient.historyList}
-                ></PatientHistoryGeneric>
-              </Paper>
-            </Grid>
-          </Grid>
-        </Form>
+    <React.Fragment>
+      {error && (
+        <CustomAlertError message={customError.message}></CustomAlertError>
       )}
-    </Formik>
+      {loading && <Loading></Loading>}
+      <Formik
+        initialValues={appointment}
+        validationSchema={validationSchema}
+        enableReinitialize
+        onSubmit={(values, actions) => {
+          if (!edit) {
+            values.person = id;
+          }
+          if (edit) {
+            (async () => {
+              try {
+                await axios.put<Appointment>(patientUrl, values);
+                history.push(patientUrl);
+              } catch (error) {
+                console.log(error);
+              }
+            })();
+          } else {
+            (async () => {
+              try {
+                await axios.post<Appointment>(appointmentsUrl, values);
+                history.push(patientUrl);
+              } catch (error) {
+                console.log(error);
+              }
+            })();
+          }
+        }}
+      >
+        {(formik) => (
+          <Form className={globalClasses.root} autoComplete="off">
+            <Grid container spacing={1}>
+              <Grid item xs={12} md={12} lg={8}>
+                <Paper className={globalClasses.paper}>
+                  <Typography component="h2" variant="h6" color="primary">
+                    {edit ? "Editar" : "Nueva"} consulta
+                  </Typography>
+                  <FormControl fullWidth className={globalClasses.root}>
+                    <TextField
+                      variant="filled"
+                      id="title"
+                      name="title"
+                      label="Motivo *"
+                      value={formik.values.title}
+                      onChange={formik.handleChange}
+                      error={
+                        formik.touched.title && Boolean(formik.errors.title)
+                      }
+                      helperText={formik.touched.title && formik.errors.title}
+                    />
+
+                    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                      <KeyboardDatePicker
+                        autoOk
+                        id="scheduled"
+                        name="scheduled"
+                        inputVariant="filled"
+                        variant="inline"
+                        format="dd/MM/yyyy"
+                        margin="normal"
+                        label="Fecha"
+                        placeholder="dd/mm/yyyy"
+                        value={formik.values.scheduled}
+                        onChange={(value) =>
+                          formik.setFieldValue("scheduled", value)
+                        }
+                      />
+                    </MuiPickersUtilsProvider>
+
+                    <FormControl
+                      variant="filled"
+                      className={globalClasses.spacing}
+                    >
+                      <InputLabel id="category-select-label">Tipo</InputLabel>
+                      <Select
+                        labelId="category-select-label"
+                        id="category-select"
+                        label="Tipo"
+                        name="category"
+                        value={formik.values.category}
+                        onChange={formik.handleChange}
+                      >
+                        <MenuItem value={AppointmentType.PreliminaryTalk}>
+                          Charla preliminar
+                        </MenuItem>
+                        <MenuItem value={AppointmentType.Surgery}>
+                          Cirugía
+                        </MenuItem>
+                        <MenuItem value={AppointmentType.Check}>
+                          Revisión
+                        </MenuItem>
+                        <MenuItem value={AppointmentType.Other}>Otro</MenuItem>
+                      </Select>
+                    </FormControl>
+
+                    <TextField
+                      variant="filled"
+                      id="description"
+                      name="description"
+                      label="Description"
+                      multiline
+                      rows={4}
+                      value={formik.values.description}
+                      onChange={formik.handleChange}
+                    />
+                    <TextField
+                      variant="filled"
+                      id="analysis"
+                      name="analysis"
+                      label="Exámen físico"
+                      multiline
+                      rows={4}
+                      value={formik.values.analysis}
+                      onChange={formik.handleChange}
+                    />
+                    <TextField
+                      variant="filled"
+                      id="plan"
+                      name="plan"
+                      label="Plan"
+                      multiline
+                      rows={10}
+                      value={formik.values.plan}
+                      onChange={formik.handleChange}
+                    />
+                    <TextField
+                      variant="filled"
+                      id="comment"
+                      name="comment"
+                      label="Observaciones"
+                      multiline
+                      rows={4}
+                      value={formik.values.comment}
+                      onChange={formik.handleChange}
+                    />
+                    <TextField
+                      variant="filled"
+                      id="recommendation"
+                      name="recommendation"
+                      label="Recomendaciones"
+                      multiline
+                      rows={4}
+                      value={formik.values.recommendation}
+                      onChange={formik.handleChange}
+                    />
+                  </FormControl>
+                </Paper>
+                <Paper className={globalClasses.paper}>
+                  <Typography component="h2" variant="h6" color="primary">
+                    Fotos (Antes)
+                  </Typography>
+                  <DropzoneArea
+                    acceptedFiles={["image/*"]}
+                    dropzoneText={"Drag and drop an image here or click"}
+                    onChange={(files) => console.log("Files:", files)}
+                    filesLimit={1}
+                  />
+                </Paper>
+                <Paper className={globalClasses.paper}>
+                  <Typography component="h2" variant="h6" color="primary">
+                    Fotos (Después)
+                  </Typography>
+                  <DropzoneArea
+                    acceptedFiles={["image/*"]}
+                    dropzoneText={"Drag and drop an image here or click"}
+                    onChange={(files) => console.log("Files:", files)}
+                    filesLimit={2}
+                  />
+                </Paper>
+                <Paper className={globalClasses.paper}>
+                  <Typography component="h2" variant="h6" color="primary">
+                    Archivos
+                  </Typography>
+                  ^{" "}
+                  <DropzoneArea
+                    acceptedFiles={["image/*"]}
+                    dropzoneText={"Drag and drop an image here or click"}
+                    onChange={(files) => console.log("Files:", files)}
+                    filesLimit={4}
+                    showFileNames={true}
+                  />
+                </Paper>
+                <Box className={globalClasses.spacing}>
+                  <Button
+                    color="primary"
+                    variant="contained"
+                    size="large"
+                    type="submit"
+                    disabled={formik.isSubmitting}
+                  >
+                    Guardar
+                  </Button>
+                  <Button
+                    variant="outlined"
+                    size="large"
+                    component={RouterLink}
+                    to={`/questionnaire`}
+                    disabled={formik.isSubmitting}
+                  >
+                    Cancelar
+                  </Button>
+                </Box>
+              </Grid>
+              <Grid item xs={12} md={12} lg={4}>
+                <PersonInfoCompact person={patient}></PersonInfoCompact>
+
+                <Paper className={globalClasses.paper}>
+                  <Typography component="h2" variant="h6" color="primary">
+                    Antecedentes
+                  </Typography>
+                  <PatientHistoryGeneric
+                    records={patient.historyList}
+                  ></PatientHistoryGeneric>
+                </Paper>
+              </Grid>
+            </Grid>
+          </Form>
+        )}
+      </Formik>
+    </React.Fragment>
   );
 }
